48282e1a895734e159c4793c5065cd497a2b89ac
Matt Christopher Romawak <mcrromawak@gmail.com>
2023-10-24 22:39:17 +0800

    halaka

---
+++ Midterm-G4-test-branch/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+<body>
+    <div>
+        <a href="Branches.php" class="btnNavigation">Navigate to Login</a>
+    </div>
+</body>
+</html>
---
+++ Midterm-G4-test-branch/script/navigation.js
@@ -0,0 +1,9 @@
+function toBranches() {
+    window.location.href = 'Branches.php';
+  }
+function toCartypes() {
+    window.location.href = 'Cartype.php';
+  }
+function toCustomers() {
+    window.location.href = 'Customers.php';
+  }
